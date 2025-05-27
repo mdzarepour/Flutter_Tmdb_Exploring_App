3dcd8689c1257c95085d839673372e73839f32fe
mdzarepour <mdzarepour@gmail.com>
2025-05-27 22:31:03 +0330

    fix ui data updating problem on HomeView

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -11,26 +11,28 @@ import 'package:movie_app/models/movie.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
   @override
   State<StatefulWidget> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
   late Future<List<List<Movie>>> _futureMovieLists;
-  late List<List<Movie>> moviewsList;
+
   @override
   void initState() {
     super.initState();
     _fetchMovieLists();
   }
 
-  void _fetchMovieLists() async {
-    _futureMovieLists = Future.wait([
-      MovieService().getMovieList(url: ApiStrings.popularVideosUrl),
-      MovieService().getMovieList(url: ApiStrings.ratedVideosUrl),
-      MovieService().getMovieList(url: ApiStrings.upcomingVideosUrl),
-    ]);
-    moviewsList = await _futureMovieLists;
+  void _fetchMovieLists() {
+    setState(() {
+      _futureMovieLists = Future.wait([
+        MovieService().getMovieList(url: ApiStrings.popularVideosUrl),
+        MovieService().getMovieList(url: ApiStrings.ratedVideosUrl),
+        MovieService().getMovieList(url: ApiStrings.upcomingVideosUrl),
+      ]);
+    });
   }
 
   @override
@@ -38,37 +40,42 @@ class _HomeViewState extends State<HomeView> {
     return Scaffold(
       body: Center(
         child: RefreshIndicator(
-          onRefresh: () async => [_fetchMovieLists(), showSnackbar(context)],
+          onRefresh: () async {
+            _fetchMovieLists();
+            await _futureMovieLists;
+            showSnackbar(context);
+          },
           child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
             child: SafeArea(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: FutureBuilder(
+                child: FutureBuilder<List<List<Movie>>>(
                   future: _futureMovieLists,
-                  builder:
-                      (context, snapshot) =>
-                          snapshot.hasData
-                              ? Column(
-                                spacing: 35,
-                                children: [
-                                  const HomeListViewTitle(
-                                    text: UiString.catTitle1,
-                                  ),
-                                  HomeSlider(list: moviewsList[0]),
-                                  const HomeListViewTitle(
-                                    text: UiString.catTitle3,
-                                  ),
-                                  HomeListView(list: moviewsList[1]),
-                                  const HomeListViewTitle(
-                                    text: UiString.catTitle3,
-                                  ),
-                                  HomeListView(list: moviewsList[2]),
-                                ],
-                              )
-                              : snapshot.connectionState ==
-                                  ConnectionState.waiting
-                              ? loadingWidget()
-                              : dataErrorWidget(_fetchMovieLists),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return loadingWidget();
+                    } else if (snapshot.hasError) {
+                      return dataErrorWidget(_fetchMovieLists);
+                    } else if (snapshot.hasData) {
+                      final List<List<Movie>> movieLists = snapshot.data!;
+                      return Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const HomeListViewTitle(text: UiString.catTitle1),
+                          HomeSlider(list: movieLists[0]),
+                          const SizedBox(height: 35),
+                          const HomeListViewTitle(text: UiString.catTitle2),
+                          HomeListView(list: movieLists[1]),
+                          const SizedBox(height: 35),
+                          const HomeListViewTitle(text: UiString.catTitle3),
+                          HomeListView(list: movieLists[2]),
+                        ],
+                      );
+                    } else {
+                      return const Text("No data to display.");
+                    }
+                  },
                 ),
               ),
             ),
